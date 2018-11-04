52f9dec9639f4553cad1adf1e7929e2c49648fa7
Likhitha <m.likhi@yahoo.com>
2018-11-04 22:18:56 +0000

    Created a leaderboard activity

---
+++ app/src/main/java/com/apps/likhithasai/coinz/LeaderboardActivity.kt
@@ -0,0 +1,14 @@
+package com.apps.likhithasai.coinz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard)
+    }
+
+
+}
